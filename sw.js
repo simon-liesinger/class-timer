0f1a69fff8ce07ab71804f1bbf2f4cd6c18d3dde
Simon Liesinger <36867918+simon-liesinger@users.noreply.github.com>
2026-01-25 10:25:47 +1100

    Update cache name to 'classtimer-v3'

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'classtimer-v2';
+const CACHE_NAME = 'classtimer-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
